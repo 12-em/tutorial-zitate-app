e46ccfedd0b701cd1df694f2501371b8d87c45fa
David <davidkh@live.de>
2023-11-11 21:22:41 +0100

    Added click and long click listeners for list items

--- app/src/main/java/com/example/zitate/MainActivity.java
+++ app/src/main/java/com/example/zitate/MainActivity.java
@@ -2,24 +2,33 @@ package com.example.zitate;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.zitate.model.Quote;
 import com.example.zitate.model.QuoteArrayAdapter;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private List<String> mSampleQuotes;
     private List<Quote> mQuoteList;
     private ListView mListView;
+    private QuoteArrayAdapter mQuoteArrayAdapter;
+    private Button mRefreshDataButton;
+
+    private LinearLayout mLayoutRoot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,15 +42,52 @@ public class MainActivity extends AppCompatActivity {
             setSupportActionBar(toolbar);
         }
 
+        mLayoutRoot = findViewById(R.id.layout_root);
+
         mListView = findViewById(R.id.zitate_list);
+        mRefreshDataButton = findViewById(R.id.button_refresh);
 
         setupSampleStrings();
         bindAdapter();
+
+        registerButtonListener();
+        registerListViewClickListener();
+    }
+
+    private void registerListViewClickListener() {
+        mListView.setOnItemClickListener((parent, view, position, id) -> {
+            int duration = Snackbar.LENGTH_LONG;
+            String author = mQuoteList.get(position).getQuoteAuthor();
+            String message = "click on q from " + author;
+
+            Snackbar.make(mLayoutRoot, message, duration).show();
+        });
+
+        mListView.setOnItemLongClickListener((parent, view, position, id) -> {
+            new MaterialAlertDialogBuilder(this)
+                    .setTitle(mQuoteList.get(position).getQuoteAuthor())
+                    .setMessage(mQuoteList.get(position).getQuoteText())
+                    .setPositiveButton(R.string.close, null)
+                    .show();
+
+            // Der "Lange Klick" wurde von uns behandelt, daher geben wir als Wert true zurück
+            return true;
+        });
+    }
+
+    private void registerButtonListener() {
+        mRefreshDataButton.setOnClickListener(v -> {
+            refreshListView();
+        });
+    }
+
+    private void refreshListView() {
+        Collections.shuffle(mQuoteList);
+        mQuoteArrayAdapter.notifyDataSetChanged();
     }
 
     private void setupSampleStrings() {
         String[] sampleQuotes = getResources().getStringArray(R.array.sample_quotes);
-        mSampleQuotes = new ArrayList<>(Arrays.asList(sampleQuotes));
         mQuoteList = new ArrayList<>();
 
         Quote sampleQuote = new Quote(sampleQuotes[0], "Johann Wolfgang v. Goethe", "goethe");
@@ -59,13 +105,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void bindAdapter() {
-        /*ArrayAdapter<Quote> quoteArrayAdapter =
-                new ArrayAdapter<>(
-                        this, // Die aktuelle Umgebung (diese Activity)
-                        R.layout.list_row, // Die ID des Zeilenlayouts (der XML-Layout Datei)
-                        R.id.quote_text,   // Die ID eines TextView-Elements im Zeilenlayout
-                        mQuoteList); // Beispieldaten in einer ArrayList*/
-        QuoteArrayAdapter arrayAdapter = new QuoteArrayAdapter(this, mQuoteList);
-        mListView.setAdapter(arrayAdapter);
+        mQuoteArrayAdapter = new QuoteArrayAdapter(this, mQuoteList);
+        mListView.setAdapter(mQuoteArrayAdapter);
     }
 }
